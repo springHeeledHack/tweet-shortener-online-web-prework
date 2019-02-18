c5e1b4c23823ca1647991d6eb2552489d7bdfb70
kyle smith <kylemclellan@gmail.com>
2019-02-18 21:40:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,15 +16,18 @@ end
 
 def word_substituter(input)
   input_a = input.split(" ")
+  output_a = []
   input_a.map do |val|
     if dictionary.has_key?(val)
-      puts dictionary[val]
       val = dictionary[val]
+      output_a << val
+    else 
+      output_a << val
     end
   end
-  input_s = input_a.join(" ")
-  puts input_s
-  input_s
+    output_s = output_a.join(" ")
+  puts output_s
+  output_s
 end
 
 word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
