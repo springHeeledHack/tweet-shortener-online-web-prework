7d11ef4c96a0dea954d468db0f6c8f30a32ff4a2
kyle smith <kylemclellan@gmail.com>
2019-02-18 21:37:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,6 +24,8 @@ def word_substituter(input)
     end
   end
   input_s = input_a.join(" ")
+  puts input_s
+  input_s
 end
 
 word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
